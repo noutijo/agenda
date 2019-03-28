de986b1582b8212cbc71a064571134bcafc64071
NOUMODONG TINDJONG Junior-Oréol <oreolnoumodong@gmail.com>
2019-03-28 14:52:58 +0100

    mise en place de python

---
+++ Launch.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cm.minhdu.launch;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Noumodong
+ */
+public class Launch extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        AnchorPane pan = FXMLLoader.load(getClass().getResource("/cm/minhdu/fxml/login.fxml"));
+
+        stage.initStyle(StageStyle.UNDECORATED);
+        //stage.initStyle(StageStyle.UNDECORATED);
+        stage.initStyle(StageStyle.TRANSPARENT);
+         stage.setTitle("Connexion");
+        Scene scene = new Scene(pan);
+        scene.setFill(Paint.valueOf("transparent"));
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
